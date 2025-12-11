f1452ce74f6876ebb63b871073110ac0e0ba540b
SusanaMA2 <103949963+SusanaMA2@users.noreply.github.com>
2025-12-11 11:38:36 -0500

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,27 @@
 import { reactive } from 'vue'
+import apiClient from '../services/apiClient'   // <-- IMPORTANTE
 
 export const authStore = reactive({
-  user: JSON.parse(sessionStorage.getItem('user') || null),
+  user: JSON.parse(sessionStorage.getItem('user') || "null"),
+
+  async fetchUser() {
+    try {
+      const res = await apiClient.get('/users/session')
+      const usuario = res.data.usuario
+
+      this.user = usuario
+      if (usuario) {
+        sessionStorage.setItem('user', JSON.stringify(usuario))
+      } else {
+        sessionStorage.removeItem('user')
+      }
+
+    } catch (err) {
+      console.error('Error al cargar usuario', err)
+      this.user = null
+      sessionStorage.removeItem('user')
+    }
+  },
 
   setUser(u) {
     this.user = u
@@ -11,5 +31,6 @@ export const authStore = reactive({
   logout() {
     this.user = null
     sessionStorage.removeItem('user')
+    apiClient.get('/users/logout')
   }
 })
